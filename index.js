25b892ec16eac458e364c9432609f870ea6c472d
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2023-12-01 18:16:41 +0100

    logg in as existing user, forum wit all posts, create new  post

--- index.js
+++ index.js
@@ -1,73 +1,123 @@
-let mysql = require("mysql");
 const express = require("express");
 const app = express();
 const port = 8080;
+const mysql = require("mysql");
+const fs = require("fs");
+
+app.use(express.urlencoded({ extended: true }));
 
 const httpServer = app.listen(port, function () {
   console.log(`Web server is running on port ${port}`);
 });
 
-app.use(express.urlencoded({ extended: true }));
-const fs = require("fs");
 
-  app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/login.html");
-  });
-  
-  app.use(express.urlencoded({ extended: true })); 
+// Skickar till klient sida med formulär för loggin
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/login.html");
+});
 
-  app.post("/", function (req, res) {
-    con = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "jensen2023",
+app.get("/userlogin", (req, res) => {
+  res.sendFile(__dirname + "/userlogin.html");
+});
+
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "jensen2023",
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to the database!");
+});
+
+// loggin already a user
+app.post("/userlogin", function (req, res) {
+  db.connect(function (err) {
+    let sql = `
+      SELECT name, username, password, email FROM users
+      WHERE name = '${req.body.name}' AND username = '${req.body.username}' AND password = '${req.body.password}' AND email = '${req.body.email}'
+    `;
+    console.log(sql);
+
+    db.query(sql, function (err, result) {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Server Error at loggin");
+        return;
+      }
+
+      if (result.length > 0) {
+        res.redirect("/forum");
+      } else {
+        res.status(401).send("Invalid credentials");
+      }
     });
-    con.connect(function (err) {
-      console.log("Connected to the database!");
-      let sql = `INSERT INTO users (name, username, password, email)
+  });
+});
+
+
+//loggin new user
+app.post("/", function (req, res) {
+  db.connect(function (err) {
+    let sql = `INSERT INTO users (name, username, password, email)
       VALUES ('${req.body.name}', '${req.body.username}', '${req.body.password}', '${req.body.email}')`;
-      console.log(sql);
-      con.query(sql, function (err, result) {
-        if (err) console.log(err);
-
-        res.redirect("/addpost");
-      });
-    });
-  });
-
-  app.get("/addpost", function (req, res) {
-    con = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "jensen2023",
-    });
-    con.connect(function (err) {
-      if (err) throw err;
-      con.query("SELECT created_at, heading FROM posts", function (err, result, fields) {
-        if (err) throw err;
-  
-        let output = "";
-        for (let post of result) {
-          const createdAt = new Date(post.created_at);
-          const formattedDate = createdAt.toLocaleString();
-          output += `${formattedDate}, Post: ${post.heading}<br>`;
-        }
-        res.send(`
-          <section class="forum-section">
-            <form class="new-post-form" action="/addpost" method="post">
-              <input type="text" name="username" placeholder="username">
-              <input type="text" name="newpost" placeholder="new post">
-              <button type="submit">add post</button>
-            </form>
-            <h3>Forum</h3>
-            <div id="posts-container">
-              ${output}
-            </div>
-          </section>
-        `);
-      });
-    });
-  });
+    console.log(sql);
     
+  db.query(sql, function (err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Server Error at loggin");
+      return;
+    }
+    res.redirect("/forum");
+  });
+  });
+});
+
+
+ // Display all posts at forum
+app.get("/forum", (req, res) => {
+  const sql = "SELECT * FROM posts";
+  db.query(sql, (err, posts) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    res.sendFile(__dirname + "/forum.html", { posts });
+  });
+});
+
+
+// Get all posts as json to fetch 
+app.get("/getposts", (req, res) => {
+  const sql = "SELECT * FROM posts";
+  db.query(sql, (err, posts) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Server Error get all posts");
+      return;
+    }
+    res.json(posts);
+  });
+});
+
+// Add new post
+app.post("/addpost", (req, res) => {
+  const { heading, body } = req.body;
+
+  const sql = "INSERT INTO posts (heading, body) VALUES (?, ?)";
+  db.query(sql, [heading, body], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    res.redirect("/forum");
+  });
+});
